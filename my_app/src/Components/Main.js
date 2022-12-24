1aee4b7fdb2bc66667dadea43016440ff1de4b64
LonelyStash <pixel.rulit@mail.ru>
2022-12-24 22:57:15 +0300

    restart

--- my_app/src/Components/Main.js
+++ my_app/src/Components/Main.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 const Main=()=>{
     return(
         <>
@@ -7,7 +7,7 @@ const Main=()=>{
                     <h1>Search for books</h1>
                 </div>
                 <div className="search">
-                    <input type="text" placeholder="Enter book name"/>
+                    <input type="text" placeholder="Enter Your Book Name"/>
                     <button><i class="fa-sharp fa-solid fa-magnifying-glass"></i></button>
                 </div>
             </div>
