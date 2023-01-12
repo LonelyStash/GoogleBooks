cb4b6352aaf425cf3de8ddb5ab1693b68fa3071a
LonelyStash <pixel.rulit@mail.ru>
2023-01-12 17:27:26 +0300

    added header

--- my_app/src/Components/Main.js
+++ my_app/src/Components/Main.js
@@ -10,6 +10,23 @@ const Main=()=>{
                     <input type="text" placeholder="Enter Your Book Name"/>
                     <button><i class="fa-sharp fa-solid fa-magnifying-glass"></i></button>
                 </div>
+                <div className="options">
+                    <p>Categories</p>
+                    <select name="option_1">
+                        <option value="1">all</option>
+                        <option value="2">art</option>
+                        <option value="3">biography</option>
+                        <option value="4">computers</option>
+                        <option value="5">history</option>
+                        <option value="6">medical</option>
+                        <option value="7">poetry</option>
+                    </select>
+                    <p>Sorting by</p>
+                     <select name="option_2">
+                        <option value="1">relevance</option>
+                        <option value="2">newest</option>
+                    </select>
+                </div>
             </div>
         </>
     )
